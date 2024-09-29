493f82e1eb273563b6fe34452772a378a208cae5
Yunus Emre Deligöz <ye@deligoz.me>
2024-09-29 23:33:15 +0300

    Refactor HasDescription trait for description positioning
    Simplified the HasDescription trait by consolidating description properties and handling position explicitly. This change makes it clearer and more maintainable by reducing redundancy and ensuring that the description is conditionally evaluated based on its position.

--- packages/tables/src/Columns/Concerns/HasDescription.php
+++ packages/tables/src/Columns/Concerns/HasDescription.php
@@ -7,28 +7,33 @@ use Illuminate\Contracts\Support\Htmlable;
 
 trait HasDescription
 {
-    protected string | Htmlable | Closure | null $descriptionAbove = null;
+    protected string | Htmlable | Closure | null $description = null;
 
-    protected string | Htmlable | Closure | null $descriptionBelow = null;
+    protected string | Closure | null $position = 'below';
 
     public function description(string | Htmlable | Closure | null $description, string | Closure | null $position = 'below'): static
     {
-        if ($position == 'above') {
-            $this->descriptionAbove = $description;
-        } else {
-            $this->descriptionBelow = $description;
-        }
+        $this->description = $description;
+        $this->position = $position;
 
         return $this;
     }
 
     public function getDescriptionAbove(): string | Htmlable | null
     {
-        return $this->evaluate($this->descriptionAbove);
+        if ($this->evaluate($this->position) === 'above') {
+            return $this->evaluate($this->description);
+        }
+
+        return null;
     }
 
     public function getDescriptionBelow(): string | Htmlable | null
     {
-        return $this->evaluate($this->descriptionBelow);
+        if ($this->evaluate($this->position) === 'below') {
+            return $this->evaluate($this->description);
+        }
+
+        return null;
     }
 }
